4124c296ea017c1e46f86220fc8d2ad7885bd22b
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-03-02 21:52:56 +0530

    setting up Redux Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./Login"
 import Body from "./Body"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
 
 function App() {
 
 
   return (
     <>
-      <BrowserRouter basename= "/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/signup" element={<div>HELOOOO from SIGNUP</div>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store= {appStore}>
+        <BrowserRouter basename= "/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login/>} />
+              <Route path="/signup" element={<div>HELOOOO from SIGNUP</div>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
