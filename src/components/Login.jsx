976a386eef1781d1acec80ab2c4d1b85232a4c5f
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-03-13 00:11:19 +0530

    minor fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,8 +7,8 @@ import { BASE_URL } from "../utils/constants";
 
 const Login = () =>{
 
-  const [emailId, setEmailId] = useState("somyarishi@gmail.com");
-  const [password, setPassword] = useState("Taresh@31");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [error, setError] = useState("");
